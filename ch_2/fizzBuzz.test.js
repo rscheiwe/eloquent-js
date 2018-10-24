cffc3ecc567788f28e8552f03891fe7fa3cf2e72
rscheiwe <rscheiwe@gmail.com>
2018-10-23 23:22:06 -0400

    finished

--- ch_2/fizzBuzz.test.js
+++ ch_2/fizzBuzz.test.js
@@ -1,14 +1,13 @@
 const fizzBuzz = (n) => {
+    let result = ''
     for (let i = 0; i <= n; i++) {
         let output = ''
-        if (i % 3 === 0) {
-            output += 'Fizz'
-        } 
-        if (i % 5 === 0) {
-            output += 'Buzz'
-        } 
+        if (i % 3 === 0) output += 'Fizz'
+        if (i % 5 === 0) output += 'Buzz' 
         console.log(output || i)
+        result = output
     }
+    return result
 }
 
 describe('fizzBuzz()', () => {
